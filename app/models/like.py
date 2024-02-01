46b4a8e01db514d34743d7bcaa085929df1eb994
Jael <humanerror787@gmail.com>
2024-01-31 22:31:47 -0500

    added likes and comments

---
+++ app/models/like.py
@@ -0,0 +1,22 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+
+class Like(db.Model):
+    __tablename__ = 'likes'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    post_id = db.Column(db.Integer, add_prefix_for_prod(db.ForeignKey('posts.id')))
+    user_id = db.Column(db.Integer, add_prefix_for_prod(db.ForeignKey('users.id')))
+
+    post = db.relationship('Post', back_populates='likes')
+    user = db.relationship('User', back_populates='likes')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'post_id': self.post_id,
+            'user_id': self.user_id
+        }
