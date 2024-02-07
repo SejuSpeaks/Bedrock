769a9966bdf06e9c7328b6f1190ce9b2f9a45391
Jael Bueno <80376392+SejuSpeaks@users.noreply.github.com>
2024-02-06 23:46:54 -0500

    template for aws plus songs route changed (#15)
    Co-authored-by: Jael <humanerror787@gmail.com>

--- app/forms/song_form.py
+++ app/forms/song_form.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, DateField
+from flask_wtf.file import FileField, FileAllowed, FileRequired
 from wtforms.validators import DataRequired, Length
+from ..api.aws_s3 import ALLOWED_EXTENSIONS
 
 class SongForm(FlaskForm):
-    url = StringField("url", validators=[DataRequired()])
+    song_file = FileField("url", validators=[FileRequired(), FileAllowed(list(ALLOWED_EXTENSIONS))])
     name = StringField('name', validators=[DataRequired()])
