1b0a8230d0bf021cc6dc8ad593e2e15c726600c0
Jael Bueno <80376392+SejuSpeaks@users.noreply.github.com>
2024-02-17 16:46:28 -0500

    delete an album (#33)
    Co-authored-by: Jael <humanerror787@gmail.com>

---
+++ react-app/src/components/DeleteAlbum/index.js
@@ -0,0 +1,26 @@
+import { useDispatch } from "react-redux";
+import { fetchDeleteAlbum } from "../../store/albums";
+import { useModal } from "../../context/Modal";
+
+
+const DeleteAlbum = ({ album_id, setUpdateAlbumList }) => {
+    const { closeModal } = useModal()
+    const dispatch = useDispatch()
+
+    const deleteAlbum = async (e) => {
+        e.preventDefault()
+        dispatch(fetchDeleteAlbum(album_id))
+            .then(() => setUpdateAlbumList(true))
+            .then(() => setUpdateAlbumList(false))
+            .then(closeModal)
+    }
+
+    return (
+        <div>
+            <button onClick={(e) => deleteAlbum(e)}>Yes Delete Album</button>
+            <button>No Keep Album</button>
+        </div>
+    );
+}
+
+export default DeleteAlbum
