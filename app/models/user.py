2a15f838afdd770a93a6ce28dd7c42c024417167
Jael <humanerror787@gmail.com>
2024-02-20 17:24:42 -0500

    took nullable off on artistaccount

--- app/models/user.py
+++ app/models/user.py
@@ -13,7 +13,7 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
-    artist_account = db.Column(db.BOOLEAN, nullable=False, default=False)
+    artist_account = db.Column(db.BOOLEAN, default=False)
     artist_name = db.Column(db.String(255), unique=True)
     bio = db.Column(db.String(255))
     profile_picture = db.Column(db.String)
